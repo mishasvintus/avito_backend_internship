7f4bb082cf6bfc83dcb9cf52a47a3cff24ab0420
Mikhail Goncharov <thinkgoncharov@gmail.ocm>
2025-11-16 23:17:37 +0300

    small fix

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -27,7 +27,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	defer db.Close()
+	defer func() { _ = db.Close() }()
 
 	reviewerAssigner := service.NewReviewerAssigner()
 	teamService := service.NewTeamService(db)
