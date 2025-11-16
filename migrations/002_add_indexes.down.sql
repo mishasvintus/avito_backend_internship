fbb93894a18c45c8a995b619d8f29ebf1111cb95
Mikhail Goncharov <thinkgoncharov@gmail.ocm>
2025-11-16 14:58:16 +0300

    added indexes for principal sql resquests

---
+++ migrations/002_add_indexes.down.sql
@@ -0,0 +1,8 @@
+-- Drop indexes
+
+DROP INDEX IF EXISTS idx_pull_requests_created_at;
+DROP INDEX IF EXISTS idx_pr_reviewers_user_id;
+DROP INDEX IF EXISTS idx_pr_reviewers_pull_request_id;
+DROP INDEX IF EXISTS idx_users_team_name_is_active;
+DROP INDEX IF EXISTS idx_users_team_name;
+
---
+++ migrations/002_add_indexes.up.sql
@@ -0,0 +1,15 @@
+-- team.Get() - WHERE team_name = $1
+CREATE INDEX IF NOT EXISTS idx_users_team_name ON users(team_name);
+
+-- user.GetActiveTeammates() - JOIN and filter by is_active
+CREATE INDEX IF NOT EXISTS idx_users_team_name_is_active ON users(team_name, is_active);
+
+-- pr.Get() - WHERE pull_request_id = $1
+CREATE INDEX IF NOT EXISTS idx_pr_reviewers_pull_request_id ON pr_reviewers(pull_request_id);
+
+-- pr.GetByUser() - JOIN and WHERE rev.user_id = $1
+CREATE INDEX IF NOT EXISTS idx_pr_reviewers_user_id ON pr_reviewers(user_id);
+
+-- pr.GetByUser() - ORDER BY pr.created_at DESC
+CREATE INDEX IF NOT EXISTS idx_pull_requests_created_at ON pull_requests(created_at DESC);
+
